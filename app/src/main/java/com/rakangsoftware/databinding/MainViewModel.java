ed6b410df06ff353caa4a922f813f771245d84ba
“Per-Erik <bergman@uncle.se>
2018-02-12 18:21:32 +0100

    Added project.

--- app/src/main/java/com/rakangsoftware/databinding/MainViewModel.java
+++ app/src/main/java/com/rakangsoftware/databinding/MainViewModel.java
@@ -2,8 +2,6 @@ package com.rakangsoftware.databinding;
 
 import android.app.Application;
 import android.arch.lifecycle.AndroidViewModel;
-import android.arch.lifecycle.MutableLiveData;
-import android.util.Log;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -14,30 +12,18 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.lang.reflect.Type;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
-import me.tatarka.bindingcollectionadapter2.ItemBinding;
-
 public class MainViewModel extends AndroidViewModel {
-    private static final String TAG = MainViewModel.class.getSimpleName();
 
     UserClickListener mUserClickListener = new UserClickListener() {
         @Override
         public void onUserClicked(final User u) {
-            Log.d(TAG, "onUserClicked() called with: user = [" + user + "]");
-            user.postValue(u);
+
         }
     };
-
-    public MutableLiveData<List<User>> items       = new MutableLiveData<>();
-    public ItemBinding<User>           itemBinding =
-            ItemBinding.<User>of(BR.user, R.layout.list_item)
-            .bindExtra(BR.listener, mUserClickListener);
-
-    public MutableLiveData<User> user = new MutableLiveData<>();
 
     private Executor mExecutor = Executors.newSingleThreadExecutor();
 
@@ -52,12 +38,10 @@ public class MainViewModel extends AndroidViewModel {
                 Gson        gson = new Gson();
                 Type listType = new TypeToken<List<User>>() {
                 }.getType();
-                List<User> obj = gson.fromJson(rd, listType);
-                items.postValue(obj);
+                List<User> userList  = gson.fromJson(rd, listType);
+
             }
         });
-
-        items.setValue(new ArrayList<User>());
     }
 
 }
