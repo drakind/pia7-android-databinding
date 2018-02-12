644a02d0bc99f276b02811a1a04ba1aea43bb2fe
“Per-Erik <bergman@uncle.se>
2018-02-12 18:28:13 +0100

    Removed databinding.

--- app/src/main/java/com/rakangsoftware/databinding/MainActivity.java
+++ app/src/main/java/com/rakangsoftware/databinding/MainActivity.java
@@ -1,26 +1,15 @@
 package com.rakangsoftware.databinding;
 
 import android.arch.lifecycle.ViewModelProviders;
-import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.rakangsoftware.databinding.databinding.ActivityMainBinding;
-
 public class MainActivity extends AppCompatActivity {
-
-    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_main);
         MainViewModel viewModel = ViewModelProviders.of(this).get(MainViewModel.class);
-
-
-
-        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        binding.setLifecycleOwner(this);
-        binding.setViewModel(viewModel);
     }
 }
